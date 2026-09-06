2bcf05a4c8e50cf7d1b4a64b11e1cbfd23cefb0b
Nassim <113632528+nassim-ou@users.noreply.github.com>
2026-09-06 18:56:41 +0100

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,9 +1,10 @@
+// src/app.js
 function greet(name) {
-    return 'Hello, ${name}!';
+    return `Hello, ${name}!`;
 }
 
 module.exports = greet;
 
-if (require.main == module){
+if (require.main === module) {
     console.log(greet('world'));
 }
